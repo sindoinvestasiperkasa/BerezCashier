4ba839b79c71495945916a41ae563a2ad180fb68
SINDO INVESTASI PERKASA <sindoinvestasiperkasa@gmail.com>
2025-07-24 19:28:35 +0000

    tolong kamu perbaiki semua dengan ini apiKey: "AIzaSyCOrDrR-0t2WAOiM1s8M

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,10 +5,10 @@ import { getFirestore, type Firestore } from "firebase/firestore";
 import { getAuth, type Auth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOrDrR-0t2WA0iM1s8MGZcQBf4L5VEiqY",
-  authDomain: "berez-04d60.firebaseapp.com",
-  projectId: "berez-04d60",
-  storageBucket: "berez-04d60.firebasestorage.app",
+  apiKey: "AIzaSyCOrDrR-0t2WAOiM1s8MGZcQBf4L5VEiqY",
+  authDomain: "berez-o4d60.firebaseapp.com",
+  projectId: "berez-o4d60",
+  storageBucket: "berez-o4d60.firebasestorage.app",
   messagingSenderId: "892398272290",
   appId: "1:892398272290:web:1758dee6a40f447e816f7b",
 };
